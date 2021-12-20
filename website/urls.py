6dfc8960ab8568e59570b7acccd91e231c3b61dc
jlcaw <jlcawarner2011@yahoo.com>
2021-12-20 08:15:11 -0700

    set up recipe site

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.index, name='index'),
+    path('gallery/<str:food>', views.gallery, name='gallery'),
+    path('blog/', views.blog, name='blog'),
+    path('recipe/', views.recipe, name='recipe'),
+    path('search/', views.search, name='search'),
+    path('random/', views.random, name='random'),
+    path('random-recipe/', views.random_recipe, name='random_recipe'),
+    path('menu/', views.menu, name='menu'),
+    path('meats/<int:id>', views.meats, name='meats')
+
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,120 @@
 from django.shortcuts import render
+import requests
+import json
 
-# Create your views here.
-def home(request):
-	return render(request, 'home.html', {})
+
+def index(request):
+	return render(request, 'index.html', {})
+
+def gallery(request, food):
+	url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search"
+	querystring = {"query": food,
+				   "number": "12",
+				   "offset": "0",
+				   "type": "main course"}
+	headers = {
+		'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		'x-rapidapi-key': "ef40eaf520msh733eda9215e1f99p16edb1jsn67d51e5b20f8"
+	}
+	response = requests.request("GET", url, headers=headers, params=querystring)
+	recipes = json.loads(response.content)
+
+
+	#grab id of recipes
+	images = []
+	row = [0,1]
+	menu_id = []
+	for item in recipes['results']:
+		id = item['id']
+		menu_id.append(id)
+		image_url = f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{id}/information"
+		image_response = requests.request("GET", image_url, headers=headers, params=querystring)
+		image = json.loads(image_response.content)
+		images.append(image['image'])
+
+	return render(request, 'gallery.html', {'images':images, 'row':row, 'menu_id': menu_id})
+
+def blog(request):
+	return render(request, 'blog.html', {})
+
+def recipe(request):
+	if request.method == 'POST':
+		recipe_search = request.POST.get('recipe').upper()
+
+		# search box
+
+		url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search"
+		querystring = {"query": recipe_search,
+					   "number": "1",
+					   "offset": "0",
+					   "type": "main course"}
+		headers = {
+			'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+			'x-rapidapi-key': "ef40eaf520msh733eda9215e1f99p16edb1jsn67d51e5b20f8"
+		}
+		response = requests.request("GET", url, headers=headers, params=querystring)
+		search = json.loads(response.content)
+
+
+		# get recipe information
+		for item in search['results']:
+			recipe_id = item['id']
+
+		url = f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{recipe_id}/information"
+
+		information_response = requests.request("GET", url, headers=headers)
+		information = json.loads(information_response.content)
+
+
+	return render(request, 'recipe.html', {'recipe': recipe_search, 'search':search['results'], 'information':information})
+
+def search(request):
+
+	return render(request, 'search.html', {})
+
+def random(request):
+	return render(request, 'random.html', {})
+
+def random_recipe(request):
+	# get recipe information
+
+	headers = {
+		'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		'x-rapidapi-key': "ef40eaf520msh733eda9215e1f99p16edb1jsn67d51e5b20f8"
+	}
+	# get random recipe
+	url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random"
+
+	querystring = { "number": "1"} # number of random recipes
+
+	headers = {
+		'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		'x-rapidapi-key': "ef40eaf520msh733eda9215e1f99p16edb1jsn67d51e5b20f8"
+	}
+
+	response = requests.request("GET", url, headers=headers, params=querystring)
+	random = json.loads(response.text)
+
+
+	information_response = requests.request("GET", url, headers=headers)
+	information = json.loads(information_response.content)
+	# print(information)
+	#
+	return render(request, 'random-recipe.html', {'random':random['recipes']})
+
+def menu(request):
+	return render(request, 'menu.html', {})
+
+def meats(request, id):
+	url = f"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{id}/information"
+
+	headers = {
+		'x-rapidapi-host': "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+		'x-rapidapi-key': "ef40eaf520msh733eda9215e1f99p16edb1jsn67d51e5b20f8"
+	}
+	response = requests.request("GET", url, headers=headers)
+	recipes = json.loads(response.content)
+	print(recipes['title'])
+
+	return render(request, 'meats.html', {'recipes':recipes})
+
