5f118ff141bfed5fdcbe6182548b229312a7ef15
Jake Warner <jcaprogamming@gmail.com>
2022-01-14 11:12:11 -0700

    set up env variables and removed login

--- recipewebsite/settings.py
+++ recipewebsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*n%9zu0d78!dfkznivh&o2%zr#60f7pbv$u0l1u()qae(6)5$_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
